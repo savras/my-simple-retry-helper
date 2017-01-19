1a21b6f329c479da20054effbfea1d7595f005b1
Vincent Gim <vgim@sateva.com.au>
2017-01-19 08:42:53 +0800

    Initial commit for retryHelper

---
+++ IRetryHelperOperation.cs
@@ -0,0 +1,6 @@
+﻿namespace RetryHelper
+{
+    public interface IRetryHelperOperation
+    {
+    }
+}
---
+++ RetryHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace RetryHelper
+{
+    public static class RetryHelper
+    {
+        private static int _retryHelperAttempts;
+        private static int _retryHelperDelay;
+
+        private static void InitializeSettings()
+        {
+            if (_retryHelperAttempts != default(int) && _retryHelperDelay != default(int)) { return; }  // Already initialized
+
+            const string retryHelperAttempts = "retryHelperAttempts";
+            const string retryHelperDelay = "retryHelperDelay";
+            const int defaultAttempts = 3;
+            const int defaultDelay = 5000;
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            int attempts;
+            int delay;
+            _retryHelperAttempts = int.TryParse(appSettings[retryHelperAttempts], out attempts) ? attempts : defaultAttempts;
+            _retryHelperDelay = int.TryParse(appSettings[retryHelperDelay], out delay) ? delay : defaultDelay;
+        }
+
+
+        public static async Task<T> CallAndRetry<T, TE>(IRetryHelperOperation operation, Func<IRetryHelperOperation, Task<T>> func)
+        {
+            InitializeSettings();
+
+            var result = default(T);
+            for (var i = 0; i < _retryHelperAttempts; i++)
+            {
+                try
+                {
+                    result = await func(operation);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is TE)
+                    {
+                        if (i < _retryHelperAttempts)
+                        {
+                            await Task.Delay(_retryHelperDelay);
+                        }
+                        else
+                        {
+                            throw;  // Let this bubble up to GlobalExceptionHandler.
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
